be1cab5018a6e8c5ea99afd9b00f401ca7d7ecef
Patrick Santos <patrickspteixeira@gmail.com>
2019-10-16 23:33:06 -0300

    adiciona middlewares

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const server = express();
 server.use(express.json())
 
 const projects = [];
+let requests = 0;
+
+server.use((_req, _res, next) => {
+    requests += 1;
+    
+    console.log('Requisições: ', requests)
+    next();
+})
+
+function projectExists(req, res, next) {
+    if (!projects.some(item => item.id == req.params.id)) {
+        return res.status(400).json({ error: 'Project not found' })
+    }
+
+    return next()
+}
 
 server.post('/projects', (req, res) => {
     const { id, title } = req.body;
@@ -18,7 +34,7 @@ server.get('/projects', (_req, res) => {
     res.json(projects)
 })
 
-server.put('/projects/:id', (req, res) => {
+server.put('/projects/:id', projectExists, (req, res) => {
     const index = projects.findIndex(item => item.id == req.params.id)
 
     projects[index].title = req.body.title
@@ -26,7 +42,7 @@ server.put('/projects/:id', (req, res) => {
     res.json(projects)
 })
 
-server.delete('/projects/:id', (req, res) => {
+server.delete('/projects/:id', projectExists, (req, res) => {
     const index = projects.findIndex(item => item.id == req.params.id)
 
     projects.splice(index, 1)
@@ -34,7 +50,7 @@ server.delete('/projects/:id', (req, res) => {
     res.send()
 })
 
-server.post('/projects/:id/tasks', (req, res) => {
+server.post('/projects/:id/tasks', projectExists, (req, res) => {
     const index = projects.findIndex(item => item.id == req.params.id)
 
     projects[index].tasks.push(req.body.title)
